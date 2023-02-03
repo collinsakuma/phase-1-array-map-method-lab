ad2a2fe308ef8025a2c7f0f185c54ec10d5c1abd
Collin Sakuma <collinmsakuma@gmail.com>
2023-02-03 13:36:08 -0800

    lab complete

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(" ")
+    return words.map(word => word[0].toUpperCase() + word.slice(1)).join(" ")
+    })
 }
+
+console.log(titleCased())
